d9c5083ca9f6e2959e0aedd06295471c8a81e2d8
DUJARDIN Fabrice <fabricedujardin873@gmail.com>
2019-10-30 19:53:43 +1100

    rajout d'un emplacement pour le tchat .(base de donnée alwaydata)

--- App/tchateTEST/body/topbar.php
+++
@@ -1,9 +0,0 @@
-<div class="topbar">
-    <a href="index.php" class="app-name">Tchat</a>
-    <span class="menu">
-        <a href="">Membres</a>
-        <a href="">Déconnexion</a>
-        <a href="">S'inscrire</a>
-        <a href="">Se connecter</a>
-    </span>
-</div>
